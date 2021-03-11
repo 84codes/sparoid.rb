de3cf405a634f6221a46437420550f2e11bf6116
Carl Hörberg <carl@84codes.com>
2021-03-11 15:06:26 +0100

    improved CLI

--- lib/sparoid/cli.rb
+++ lib/sparoid/cli.rb
@@ -6,8 +6,16 @@ require_relative "../sparoid"
 module Sparoid
   # CLI
   class CLI < Thor
-    desc "send KEY HMAC_KEY HOST PORT", "Send a packet"
-    def send(key, hmac_key, host, port)
+    desc "send HOST [PORT]", "Send a packet"
+    method_option :config, default: "~/.sparoid.ini"
+    def send(host, port = 8484)
+      if File.exist? options[:config]
+        c = parse_config(options[:config])
+        key = c["key"]
+        hmac_key = c["hmac-key"]
+      else
+        abort "Config not found"
+      end
       Sparoid.send(key, hmac_key, host, port.to_i)
     end
 
@@ -18,6 +26,12 @@ module Sparoid
 
     def self.exit_on_failure?
       true
+    end
+
+    private
+
+    def parse_config(path)
+      File.readlines(path).map! { |l| l.split("=", 2).map!(&:strip) }.to_h
     end
   end
 end
