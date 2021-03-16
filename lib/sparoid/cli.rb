9905f7944bb40bf42a9a272b6a2566abd9b3e757
Carl Hörberg <carl@84codes.com>
2021-03-16 09:26:37 +0100

    abort on exceptions in cli

--- lib/sparoid/cli.rb
+++ lib/sparoid/cli.rb
@@ -8,23 +8,22 @@ module Sparoid
   class CLI < Thor
     desc "auth HOST [PORT]", "Send a authorization packet"
     method_option :config, desc: "Path to a config file, INI format, with key and hmac-key"
-    method_option :fdpass,
-                  type: :numeric,
-                  desc: "After sending, open a TCP connection and pass the FD back to the calling process. \
-                         For use with OpenSSH ProxyCommand and ProxyUseFdpass"
     def auth(host, port = 8484)
       config = File.expand_path(options[:config] || "~/.sparoid.ini")
       abort "Config '#{config}' not found" unless File.exist? config
 
       key, hmac_key = get_keys(parse_ini(config))
       Sparoid.auth(key, hmac_key, host, port.to_i)
-      Sparoid.fdpass(host, options[:fdpass]) if options[:fdpass]
+    rescue ex
+      abort ex.message
     end
 
     desc "connect", "Send a SPA, TCP connect, and then pass the FD back to the parent"
     def connect(host, port, spa_port = 8484)
       auth(host, spa_port)
       Sparoid.fdpass(host, port)
+    rescue ex
+      abort ex.message
     end
 
     desc "keygen", "Generate an encryption key and a HMAC key"
