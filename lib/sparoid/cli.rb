d3533b9daf028ea56ce23640cf2eda559061db1e
Carl Hörberg <carl@84codes.com>
2021-03-25 11:29:22 +0100

    Only warn if config is missing on sparoid connect

--- lib/sparoid/cli.rb
+++ lib/sparoid/cli.rb
@@ -7,20 +7,23 @@ module Sparoid
   # CLI
   class CLI < Thor
     desc "auth HOST [PORT]", "Send a authorization packet"
-    method_option :config, desc: "Path to a config file, INI format, with key and hmac-key"
+    method_option :config, desc: "Path to a config file, INI format, with key and hmac-key", default: "~/.sparoid.ini"
     def auth(host, port = 8484)
-      config = File.expand_path(options[:config] || "~/.sparoid.ini")
-      abort "Config '#{config}' not found" unless File.exist? config
-
-      key, hmac_key = get_keys(parse_ini(config))
-      Sparoid.auth(key, hmac_key, host, port.to_i)
+      send_auth(host, port, options[:config])
+    rescue Errno::ENOENT
+      abort "Config not found"
     rescue StandardError => e
       abort e.message
     end
 
     desc "connect HOST PORT [SPA-PORT]", "Send a SPA, TCP connect, and then pass the FD back to the parent"
+    method_option :config, desc: "Path to a config file, INI format, with key and hmac-key", default: "~/.sparoid.ini"
     def connect(host, port, spa_port = 8484)
-      auth(host, spa_port)
+      begin
+        send_auth(host, spa_port, options[:config])
+      rescue Errno::ENOENT
+        warn "Config not found"
+      end
       Sparoid.fdpass(host, port)
     rescue StandardError => e
       abort e.message
@@ -37,8 +40,13 @@ module Sparoid
 
     private
 
+    def send_auth(host, port, config)
+      key, hmac_key = get_keys(parse_ini(config))
+      Sparoid.auth(key, hmac_key, host, port.to_i)
+    end
+
     def parse_ini(path)
-      File.readlines(path).map! { |l| l.split("=", 2).map!(&:strip) }.to_h
+      File.readlines(File.expand_path(path)).map! { |line| line.split("=", 2).map!(&:strip) }.to_h
     end
 
     def get_keys(config)
