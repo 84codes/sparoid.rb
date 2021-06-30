1572c79cc4bf9e39d410c1f93faa507d84a23bc6
Patrik Ragnarsson <patrik@starkast.net>
2021-06-30 09:40:25 +0200

    Use temporary cache file when running tests
    Not the real cache file that is used when using the Sparoid client.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "tempfile"
+ENV["SPAROID_CACHE_PATH"] ||= Tempfile.new("sparoid_cache").path
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "sparoid"
 
