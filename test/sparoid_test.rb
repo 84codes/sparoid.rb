5da82787632aec0e5721619475be065700ea324c
Anton Dalgren <anton@84codes.com>
2026-02-12 11:46:54 +0100

    disable rubocop rule

--- test/sparoid_test.rb
+++ test/sparoid_test.rb
@@ -2,7 +2,7 @@
 
 require "test_helper"
 
-class SparoidTest < Minitest::Test
+class SparoidTest < Minitest::Test # rubocop:disable Metrics/ClassLength
   def test_that_it_has_a_version_number
     refute_nil ::Sparoid::VERSION
   end
